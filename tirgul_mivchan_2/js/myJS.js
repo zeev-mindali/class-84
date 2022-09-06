ef5bb25375cf5148f65d864965115f78efa82aa3
zeev mindali <mindalizeev@gmail.com>
2022-09-06 14:06:42 +0300

    map

--- tirgul_mivchan_2/js/myJS.js
+++ tirgul_mivchan_2/js/myJS.js
@@ -14,6 +14,7 @@ $(() => {
       getNapot().map((item) => console.log(item));
     }
     console.log(myResult[1]);
+    getTotal();
   });
 });
 
@@ -25,6 +26,63 @@ const getNapot = () => {
     }
   });
   return napot;
+};
+
+const getNapotFor = () => {
+  let napot = [];
+  for (let index = 0; index < myResult.length; index++) {
+    if (
+      !myResult[index]["נפה"].includes("לא ידוע") &&
+      !napot.includes(myResult[index]["נפה"])
+    ) {
+      napot.push(myResult[index]);
+    }
+  }
+  return napot;
+};
+
+//בנייה של מבנה נתונים שאנחנו רוצים לקבל
+let singleObject = {
+  age5_0: 0,
+  age6_18: 0,
+  age19_45: 0,
+  age46_55: 0,
+  age56_64: 0,
+  age65: 0,
+  total: 0,
+  napa: "",
+};
+
+const getTotal = () => {
+  let napaTotal = [];
+  //איטרציה על כל המידע לפי שם נפה
+  getNapot().map((napaName) => {
+    //חיתוך עפ"י שם הנפה, קבלת המידע עבור נפה בודדת
+    const singleNapa = myResult.filter(
+      (singleCity) => singleCity["נפה"] == napaName
+    );
+    //יצירה של משתנה חדש עפ"י המבנה שייצרנו, שמייצג כתובת אחרת בזיכרון
+    let myData = { ...singleObject };
+    //הכנסת שם הנפה
+    myData.napa = napaName;
+
+    //איטרציה על כל היישובים שקשורים לנפה בודדת, וסיכום הערכים של הגילאים
+    singleNapa.map((singleCity) => {
+      myData.age5_0 += singleCity["גיל_0_5"];
+      myData.age6_18 += singleCity["גיל_6_18"];
+      myData.age19_45 += singleCity["גיל_19_45"];
+      myData.age46_55 += singleCity["גיל_46_55"];
+      myData.age56_64 += singleCity["גיל_56_64"];
+      myData.age65 += singleCity["גיל_65_פלוס"];
+      myData.total += singleCity["סהכ"];
+    });
+    // console.log(myData);
+    //הוספת הנפה למערך שמייצג את כל הנפות
+    napaTotal.push(myData);
+  });
+  //console.log(napaTotal);
+  //החזרת מערך הנפות מסוכם
+  return napaTotal;
 };
 
 function getData() {
