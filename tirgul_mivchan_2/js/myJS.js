9dc4ea5568da9aa0adfe0804df981131e664223c
zeev mindali <mindalizeev@gmail.com>
2022-09-06 10:17:06 +0300

    mivchan #2

---
+++ tirgul_mivchan_2/js/myJS.js
@@ -0,0 +1,28 @@
+const url =
+  "https://data.gov.il/api/3/action/datastore_search?resource_id=64edd0ee-3d5d-43ce-8562-c336c24dbc1f&q=";
+let myResult = [];
+
+// Document ready after leon will cry....
+$(() => {
+  $("#searchMe").click(() => {
+    if (!sessionStorage.getItem("data")) {
+      console.log("getting data from server...");
+      getData();
+    } else {
+      console.log("getting data from session...");
+      myResult = JSON.parse(sessionStorage.getItem("data"));
+    }
+  });
+  
+});
+
+function getData() {
+  $.ajax({
+    url: url, //+$("#city").val()
+    success: function (data) {
+      myResult = data.result.records;
+      //console.log(myResult);
+      sessionStorage.setItem("data", JSON.stringify(myResult));
+    },
+  });
+}
