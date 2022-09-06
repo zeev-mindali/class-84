b493851b3a84eda8c7bb3fb5a51afd5c258fcc7e
zeev mindali <mindalizeev@gmail.com>
2022-09-06 11:53:20 +0300

    functions

--- tirgul_mivchan_2/js/myJS.js
+++ tirgul_mivchan_2/js/myJS.js
@@ -1,7 +1,7 @@
 const url =
   "https://data.gov.il/api/3/action/datastore_search?resource_id=64edd0ee-3d5d-43ce-8562-c336c24dbc1f&q=";
 let myResult = [];
-let napot = [];
+
 // Document ready after leon will cry....
 $(() => {
   $("#searchMe").click(() => {
@@ -11,15 +11,21 @@ $(() => {
     } else {
       console.log("getting data from session...");
       myResult = JSON.parse(sessionStorage.getItem("data"));
-      console.log(myResult);
-      myResult.map((item) => {
-        napot.push(item);
-      });
-      //תרגיל כיתה - להעיף בצורה חכמה את ה לא ידוע
-      napot.map((item) => console.log(item));
+      getNapot().map((item) => console.log(item));
     }
+    console.log(myResult[1]);
   });
 });
+
+const getNapot = () => {
+  let napot = [];
+  myResult.map((item) => {
+    if (!item["נפה"].includes("לא ידוע") && !napot.includes(item["נפה"])) {
+      napot.push(item["נפה"]);
+    }
+  });
+  return napot;
+};
 
 function getData() {
   $.ajax({
