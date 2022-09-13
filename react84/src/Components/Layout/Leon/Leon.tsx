9c7ee6b3af539c152e6266bb0bbdf674ea3a77eb
zeev mindali <mindalizeev@gmail.com>
2022-09-13 15:06:48 +0300

    react

---
+++ react84/src/Components/Layout/Leon/Leon.tsx
@@ -0,0 +1,12 @@
+import "./Leon.css";
+import leon from "../../../assets/leon.png";
+
+function Leon(): JSX.Element {
+    return (
+        <div className="Leon">
+			<img src={leon} width={150}/>
+        </div>
+    );
+}
+
+export default Leon;
