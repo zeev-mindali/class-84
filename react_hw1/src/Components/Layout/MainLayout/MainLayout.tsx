be06a2f9d0b5ad217fb8b4d7545d9f219bbf0c9f
zeev mindali <mindalizeev@gmail.com>
2022-09-18 10:57:08 +0300

    useState

--- react_hw1/src/Components/Layout/MainLayout/MainLayout.tsx
+++ react_hw1/src/Components/Layout/MainLayout/MainLayout.tsx
@@ -1,3 +1,5 @@
+import { SyntheticEvent, useState } from "react";
+import { SyntheticExpression } from "typescript";
 import Details from "../Details/Details";
 import Header from "../Header/Header";
 import Skills from "../Skills/Skills";
@@ -5,11 +7,25 @@ import "./MainLayout.css";
 
 function MainLayout(): JSX.Element {
     let killName = "Bill";
+    //let myKiller ="Zeev";
+    const [myKiller,setKiller] = useState("wait for killer name");
+    // const killChange = (args:SyntheticEvent)=>{
+    //     const myValue = (args.target as HTMLInputElement).value;
+    //     console.log(myValue);
+
+    // };
+    function killChange(args:SyntheticEvent){
+        const myValue = (args.target as HTMLInputElement).value;
+        console.log(myValue);
+        //myKiller = myValue;
+        setKiller(myValue)
+    }
     return (
         <div className="MainLayout">
             <div id="getKiller">
-                <input type="text" id="killerName" placeholder="enter killer name" />
+                <input type="text" id="killerName" placeholder="enter killer name" onChange={killChange}/>
                 <input type="button" value="change" />
+                <div id="res">{myKiller}</div>
             </div>
 			<header><Header killName={killName}/></header>
             <div id="Details"><Details killName={killName}/></div>
