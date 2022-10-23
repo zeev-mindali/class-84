3c9a7a3cabda586f9828d912c5d8b3366cd7a75b
zeev mindali <mindalizeev@gmail.com>
2022-10-23 11:26:46 +0300

    mui

--- src/Components/Layout/GoodForm/GoodForm.tsx
+++ src/Components/Layout/GoodForm/GoodForm.tsx
@@ -1,47 +1,76 @@
 import { useForm } from "react-hook-form";
+import notify from "../../../utils/Notify";
 import "./GoodForm.css";
 import { UserCred } from "./userCred";
 
 function GoodForm(): JSX.Element {
-    //npm install react-hook-form
-    const {register,handleSubmit,formState:{errors}} = useForm<UserCred>();
+  //npm install react-hook-form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserCred>();
 
-    const send = (userCred:UserCred)=>{
-        console.log(userCred);
-        //axios send, post to server
-    };
-    return (
-        <div className="GoodForm">
-            <form onSubmit={handleSubmit(send)}>
-                <h1>Login Form<br/>Good Version</h1><hr/>
-                <label>Name:</label>
-                <input type="Text" placeholder="User name.." {...register("userName",{
-                    required:{
-                        value:true,
-                        message: "יש להקיש שם משתמש"
-                    },
-                    minLength:{
-                        value:3,
-                        message:"מינימום 3 תווים"
-                    }
-                })}/>
-                <span>{errors.userName?.message}</span><br/><br/>
-                <label>Password:</label>
-                <input type="password" placeholder="Password..." {...register("userPassword",{
-                     required:{
-                        value:true,
-                        message: "יש להקיש שם משתמש"
-                    },
-                    minLength:{
-                        value:3,
-                        message:"מינימום 3 תווים"
-                    }
-                })}/>
-                {errors.userPassword && <span>in your mother, what with password?</span>}
-                <button>login</button>
-            </form>
-        </div>
-    );
+  const send = (userCred: UserCred) => {
+    console.log(userCred);
+    if (userCred.userName === "zeev") {
+      console.log("OK");
+      notify.success("User has logged in successfully");      
+    } else {
+      console.log("Error");
+      notify.error("Why who are you?");
+    }
+    //axios send, post to server
+  };
+  return (
+    <div className="GoodForm">
+      <form onSubmit={handleSubmit(send)}>
+        <h1>
+          Login Form
+          <br />
+          Good Version
+        </h1>
+        <hr />
+        <label>Name:</label>
+        <input
+          type="Text"
+          placeholder="User name.."
+          {...register("userName", {
+            required: {
+              value: true,
+              message: "יש להקיש שם משתמש",
+            },
+            minLength: {
+              value: 3,
+              message: "מינימום 3 תווים",
+            },
+          })}
+        />
+        <span>{errors.userName?.message}</span>
+        <br />
+        <br />
+        <label>Password:</label>
+        <input
+          type="password"
+          placeholder="Password..."
+          {...register("userPassword", {
+            required: {
+              value: true,
+              message: "יש להקיש שם משתמש",
+            },
+            minLength: {
+              value: 3,
+              message: "מינימום 3 תווים",
+            },
+          })}
+        />
+        {errors.userPassword && (
+          <span>in your mother, what with password?</span>
+        )}
+        <button>login</button>
+      </form>
+    </div>
+  );
 }
 
 export default GoodForm;
--- src/Components/cars/routing/routing.tsx
+++ src/Components/cars/routing/routing.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import BadForm from "../../Layout/Form/BadForm";
 import Form from "../../Layout/Form/BadForm";
 import GoodForm from "../../Layout/GoodForm/GoodForm";
+import MuiForm from "../../Layout/MuiForm/MuiForm";
 import CarLocator from "../CarLocator/CarLocator";
 import Findbike from "../findbike/findbike";
 import FindHandiCap from "../findHandiCap/findHandiCap";
@@ -12,23 +13,24 @@ import FindTruck from "../findTruck/findTruck";
 import Page404 from "../Page404/Page404";
 
 function Routing(): JSX.Element {
-    return (
-        <div className="routing">
-			<Routes>
-                <Route path="/" element={<FindMain/>}/>                
-                <Route path="/car" element={<CarLocator/>}/>
-                {/*  Single car passing paramters to Component*/}
-                <Route path="/bike" element={<Findbike/>}/>
-                <Route path="/truck" element={<FindTruck/>}/>
-                <Route path="/offRoad" element={<FindOffRoad/>}/>
-                <Route path="/handiCap" element={<FindHandiCap/>}/>
-                <Route path="/recall" element={<FindRecall/>}/>
-                <Route path="/badForm" element={<BadForm/>}/>
-                <Route path="/goodForm" element={<GoodForm/>}/>
-                <Route path="*" element={<Page404/>}/>
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="routing">
+      <Routes>
+        <Route path="/" element={<FindMain />} />
+        <Route path="/car" element={<CarLocator />} />
+        {/*  Single car passing paramters to Component*/}
+        <Route path="/bike" element={<Findbike />} />
+        <Route path="/truck" element={<FindTruck />} />
+        <Route path="/offRoad" element={<FindOffRoad />} />
+        <Route path="/handiCap" element={<FindHandiCap />} />
+        <Route path="/recall" element={<FindRecall />} />
+        <Route path="/badForm" element={<BadForm />} />
+        <Route path="/goodForm" element={<GoodForm />} />
+        <Route path="/coolForm" element={<MuiForm />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default Routing;
