b46a87e4a2ac98e8be06435541813ad30f910f68
zeev mindali <mindalizeev@gmail.com>
2022-11-06 11:44:53 +0200

    moved to arrow functions

--- fullstack/backend/Logic/logic.ts
+++ fullstack/backend/Logic/logic.ts
@@ -10,52 +10,52 @@ function getDonations() {
 }
 
 //get single donation
-function getSingleDonation(id: number) {
+const getSingleDonation = (id: number) => {
   return donationList.filter((item) => item.getID() === id);
-}
+};
 //add donation
-function addDonation(newDonation: Donation) {
+const addDonation = (newDonation: Donation) => {
   //const myDonation = new Donation();
   //myDonation.getDonation(newDonation);
   //donationList.push(myDonation);
   donationList.push(newDonation);
-}
+};
 
 //delete donation
-function deleteID(id: number) {
+const deleteID = (id: number) => {
   //donationList = donationList.filter((item) => item.getID() !== id); will not work becuase it's an import
   deleteME(id);
-}
+};
 
 //update donation
 
 //cancel donation
 
 //get donation range by sum
-function getDonationRange(from: number, to: number) {
+const getDonationRange = (from: number, to: number) => {
   return donationList.filter(
     (item) => item.getSum() >= from && item.getSum() <= to
   );
-}
+};
 
 //get donation by name
-function getDonationByName(userName: string) {
+const getDonationByName = (userName: string) => {
   return donationList.filter(
     (item) =>
       item.getName().includes(userName) || item.getLastName().includes(userName)
   );
-}
+};
 
 //testmode
-function makeTest() {
+const makeTest = () => {
   return { name: "Leon", moto: "mess with the best and die like the rest" };
-}
-function sayHi(userName: string, age: number) {
+};
+const sayHi = (userName: string, age: number) => {
   return {
     name: "Hello " + userName,
     born: 2022 - age,
   };
-}
+};
 
 export default {
   makeTest,
