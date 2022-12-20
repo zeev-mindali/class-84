a8b4edbd51a4bc92fc732dac83c3c88205a51bb0
zeev mindali <mindalizeev@gmail.com>
2022-12-20 08:56:33 +0200

    search

--- php/exam/MySearch.php
+++ php/exam/MySearch.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="he">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>I am google :)</title>
     
+    <title>I am google :)</title>
+    <style>
+        body{
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    <center>
         <h1>PHP search</h1>
         <label> In your mother in law, what you want?</label>
         <form method="get" action="">
             <input type="text" name="results"/>
             <input type="submit"/>
         </form>
-        <?php            
-            if (isset($_GET['results']) && $_GET['result'] != ""){
+        <?php         
+          
                 echo "<br/>tour search result array:<hr/><br/>";
-                $url = "https://www.google.co.il/search?q=".str_replace(' ','%20',$_get['results']);
+                $url = "https://www.google.co.il/search?q=".str_replace(' ','%20',$_GET['results']);
 
                 //zeev mindali->zeev%20mindali
 
@@ -33,19 +37,13 @@
                 //execute the command
                 $body = curl_exec($ch);
                 //close the the connection
-                curl_close();
+                curl_close($ch);
 
-                //now precess the json string
-                $json = json_decode($body);
-
-                //print out the arry               
-                foreach ($json as $item){
-                    echo "$item<br/>";
-                }
-            }
+                //encode to hebrew
+                $myResult = utf8_decode($body);
+                //show the result
+        
 
         ?>
-
-    </center>
 </body>
 </html>
