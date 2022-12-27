b4ed35288ff7029bed997c7365ac004c1508e382
zeev mindali <mindalizeev@gmail.com>
2022-12-27 13:50:02 +0200

    storybook

--- zeev_storybook/presso_sb/src/Button.tsx
+++ zeev_storybook/presso_sb/src/Button.tsx
@@ -1,30 +1,27 @@
-import React, { HTMLAttributes, ReactNode } from "react";
+import React, {HTMLAttributes, ReactNode} from 'react';
 
 //extends to send specific attributes of button element
-export interface Props extends HTMLAttributes<HTMLButtonElement>{ //sending a specific props to a button element
-    /** Provide a text for the button (3) */
-    children: ReactNode; /* can be Element,string,number,array,etc... */
-    /** Which variant look would you like to use */
-    variant: 'primary' | 'secondary'; //mui,bs,etc...
+export interface Props extends HTMLAttributes<HTMLButtonElement>{
+    // provide a text for the button
+    children: ReactNode; // can be Element,String,number,array,etc....
+    // which variant look would you like to use
+    variant: 'primary' | 'secondary'; //MUI,BS,Chakra
 }
 
-/** This is a special button !!!! */ 
-//the variant will be added when we add style
-export const Button = ({children,variant='primary',...props}: Props) => {
+//this is a special button
+export const Button  = ({children, variant='primary',...props}:Props) => {
     return (
-        //style when creating second story
-        <button {...props} style={{  //css,sass,css in js...
-            backgroundColor: variant==='primary'?"blue":"gray",
+        <button {...props} style={{  //css,ass,css in js
+            backgroundColor:variant ==='primary' ? 'blue':"gray",
             color:"white",
-            border: "none",
+            //backgroundColor:"black",
+            //color:"darkblue",
+            border:"none",
             borderRadius: 100,
             padding: 10,
             cursor: 'pointer'
-            //for accessibility demo
-            //backgroundColor: "black",
-            //color:"black",
-            }}> {/* props - for sending things like event (onClick), attributes, etc... */}
-            {children}
+        }}>
+        {children}
         </button>
     )
 }
--- zeev_storybook/presso_sb/stories/Button.stories.tsx
+++ zeev_storybook/presso_sb/stories/Button.stories.tsx
@@ -1,43 +1,35 @@
 //basic imports
-import React from 'react';
-import {Meta,Story} from '@storybook/react';
+import React, { Component } from 'react';
+import {Meta, Story} from '@storybook/react'
 
-//import of our button that we created
-import {Button,Props} from '../src/Button';
-import {action} from '@storybook/addon-actions';
+//import of our button that we will create :)
+import {Button,Props} from "../src/Button";
+import {action} from "@storybook/addon-actions";
 
-//declartion of storybook data
+//declaration of storybook data
 const meta:Meta = {
-    title: 'Button', //required
-    component: Button, //the component that will be created
-    //(2)
+    title: 'Button',
+    component:Button,
     argTypes:{
-        onClick: {action: 'clicked'}, //new action, default behavior
-        children: {
-            defaultValue: 'Defualt Text',
+        onclick:{action:'clicked'},
+        children:{
+            defaultValue: 'I am a button',
         }
     }
 }
 
 export default meta;
 
-//create template (2)
+// export const Defualt= ()=><Button variant='primary'>Kill Leon</Button>
+// export const Secondary = ()=><Button variant='secondary'>i am another button</Button>
+
 const Template:Story<Props> = (args)=><Button {...args}/>
 
-////////////////////////////// the button now can be exported
-
-//export const Default = ()=><Button variant='primary'>CLICK ME</Button> 
-//create clone from line 25...
 export const Default = Template.bind({});
-//multiple buttons
-//export const Secondary = ()=><Button variant='secondary'>I am secondary button</Button>;
 export const Secondary = Template.bind({});
 
-//sending args
-
-
 Secondary.args = {
-    variant: 'secondary',
-    children: ' i am secondary button',
-    onClick: action('Secondary click'), //phase2(after showing the addons) add import
+    variant : 'secondary',
+    children: 'i am another button',
+    onClick: action('Kill Leon'),
 }
